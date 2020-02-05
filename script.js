45380458cca05eb430958bc196979ebbd41e8ab4
vjekas <vjekoslav.vuckovic@gmail.com>
2020-02-05 19:51:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomButton = document.getElementById("random");
+
+function setGradient() {
+	body.style.background =
+	"linear-gradient(to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+setGradient();
+
+/* random colors */
+function randomColors() {
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+
+	setGradient();
+}
+
+/* get random color */
+function getRandomColor() {
+	var letNum = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color = color + letNum[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+randomButton.addEventListener("click", randomColors);
